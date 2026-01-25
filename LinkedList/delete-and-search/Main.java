6605c910af525a02f2efdd8a08a6d097cb482609
Neetesh Dixit <neeteshdixit@gmail.com>
2026-01-25 19:22:19 +0530

    Implement linked list operations: insert, delete, and search methods

--- LinkedList/delete-and-search/Main.java
+++ LinkedList/delete-and-search/Main.java
@@ -1,3 +1,5 @@
+
+
 public class Main {
 
     Node head = null;
@@ -54,50 +56,7 @@ public class Main {
 
     }
 
-    // delete from beginning
-    public void deleteFromBeginning(){
-        if(head == null){
-            return;
-        }
-        head = head.next;
-    }
-
-    // delete from end
-    public void deleteFromEnd(){
-        if(head == null){
-            return;
-        }
-        if(head.next == null){
-            head = null;
-            return;
-        }
-        Node temp = head;
-        while(temp.next.next != null){
-            temp = temp.next;
-        }
-        temp.next = null;
-    }
-
-    // delete from position
-    public void deleteFromPosition(int position){
-        if(head == null){
-            return;
-        }
-        if(position == 0){
-            head = head.next;
-            return;
-        }
-        Node temp = head;
-        int index = 0;
-        while(temp != null && index < position-1){
-            temp = temp.next;
-            index++;
-        }
-        if(temp != null && temp.next != null){
-            temp.next = temp.next.next;
-        }
-    }
-
+    // display linked list
     public void display(){
         Node temp = head;
         while(temp != null){
@@ -106,28 +65,62 @@ public class Main {
         }
         System.out.println("null");
     }
-    // search element
-    public boolean search(int key){
-        Node temp = head;
-        while(temp != null){
-            if(temp.data == key){
-                return true;
-            }
-            temp = temp.next;
+
+    // delete from beginning
+    public void deleteFromBeginning(){
+        if(head==null){ 
+            System.out.println("List is empty");
+            return;
         }
-        return false;
+        head = head.next;
     }
     
-     public static void main(String[] args) {
+    // delete from end
+    public void deleteFromEnd(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        if(head.next == null){
+            head = null;
+            return;
+        }
+
+        Node temp = head;
+        while(temp.next.next!=null){
+            temp = temp.next;
+        }
+        temp.next = null;
+    }
+
+    // delete from position
+    public void deleteFromPosition(int position){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        
+        if(position == 0){
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+        int index = 0;
+        while(temp.next !=null && index < position-1){
+            temp = temp.next;
+            index++;
+        }
+        temp.next = temp.next.next;
+    }
+     
+    
+    
+    public static void main(String[] args) {
         Main list = new Main();
          list.insertAtBeginning(5);
-         list.insertAtBeginning(8);
         list.insertAtEnd(10);
-        list.insertAtEnd(18);
         list.insertAtPosition(1, 7);
-        list.deleteFromBeginning();
-        list.deleteFromEnd();
-        list.deleteFromPosition(0);
         list.display();
     }
 }
