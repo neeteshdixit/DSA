107b0bbf7d38f2fae670b466c259e7fee715eaeb
neeteshdixit <dixitneetesh857@gmail.com>
2026-02-07 14:07:21 +0530

    head should not be static

--- Stack/StackClass.java
+++ Stack/StackClass.java
@@ -1,53 +1,63 @@
-public class StackClass{
+public class StackClass {
 
-    static class Node{
+    static class Node {
         int data;
         Node next;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
-            this.next = next; 
+            this.next = null;
         }
     }
 
-    static class Stack{
-        public static Node head;
+    static class Stack {
+        private Node head;
 
-        public static boolean isEmpty(){
+        public boolean isEmpty() {
             return head == null;
         }
 
-        public static void push(int data){
+        public void push(int data) {
             Node newnode = new Node(data);
 
-            if(isEmpty()){
+            if (isEmpty()) {
                 head = newnode;
                 return;
             }
+
             newnode.next = head;
             head = newnode;
         }
 
-        public static int pop(int data){
-            if(isEmpty()){
+        public int pop() {
+            if (isEmpty()) {
                 return -1;
             }
-             int top = head.data;
-             head = head.next;
-             return top;
+
+            int top = head.data;
+            head = head.next;
+            return top;
         }
 
-        public static int peek(){
-            if(isEmpty()){
+        public int peek() {
+            if (isEmpty()) {
                 return -1;
             }
 
             return head.data;
         }
 
+        public void printStack(String label) {
+            System.out.println(label);
+            for (Node curr = head; curr != null; curr = curr.next) {
+                System.out.println(curr.data);
+            }
+        }
     }
+
     public static void main(String[] args) {
         Stack s = new Stack();
+
         s.push(1);
         s.push(2);
         s.push(3);
@@ -55,15 +65,11 @@ public class StackClass{
         s.push(5);
         s.push(6);
 
-        while(s.isEmpty()){
-            System.out.println("Before pop"+s.peek());
-        }
+        s.printStack("Before pop");
 
-        s.pop(4);
-        s.pop(5);
+        s.pop();
+        s.pop();
 
-        while(s.isEmpty()){
-            System.out.println("After pop"+s.peek());
-        }
+        s.printStack("After pop");
     }
 }
