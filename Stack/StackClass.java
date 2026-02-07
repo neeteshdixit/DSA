979b65c5172246cbc98cb310bbf5e775492c4e6c
neeteshdixit <dixitneetesh857@gmail.com>
2026-02-07 14:00:20 +0530

    stack operations

--- Stack/StackClass.java
+++ Stack/StackClass.java
@@ -28,18 +28,42 @@ public class StackClass{
             head = newnode;
         }
 
-        public static void pop(int data){
-            Node newnode = new Node(data);
-
+        public static int pop(int data){
             if(isEmpty()){
-                return;
+                return -1;
+            }
+             int top = head.data;
+             head = head.next;
+             return top;
+        }
+
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
             }
 
-            
+            return head.data;
         }
 
     }
     public static void main(String[] args) {
-        
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        s.push(6);
+
+        while(s.isEmpty()){
+            System.out.println("Before pop"+s.peek());
+        }
+
+        s.pop(4);
+        s.pop(5);
+
+        while(s.isEmpty()){
+            System.out.println("After pop"+s.peek());
+        }
     }
 }
