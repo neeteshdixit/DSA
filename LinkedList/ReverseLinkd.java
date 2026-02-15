58eb2d232461542f4206e4d7ca31fc14046b7119
neeteshdixit <dixitneetesh857@gmail.com>
2026-02-15 12:10:21 +0530

    reverse linked list

---
+++ LinkedList/ReverseLinkd.java
@@ -0,0 +1,33 @@
+class ReverseLnkd {
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode newHead = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
+    }
+
+    public static void printList(ListNode head){
+        while(head != null){
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        head = reverseList(head);
+
+        printList(head);
+    }
+}
