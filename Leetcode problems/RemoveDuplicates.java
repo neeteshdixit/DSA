0fc9bd3512125fca91355d283e3b553c4d402b6b
neeteshdixit <dixitneetesh857@gmail.com>
2026-06-02 11:22:12 +0530

    sorted array remove duplicates

--- Leetcode problems/RemoveDuplicates.java
+++ Leetcode problems/RemoveDuplicates.java
@@ -1,5 +1,5 @@
-
-import java.util.HashSet;
+// when array is unsorted or bole ki duplicate remove krdo
+ import java.util.HashSet;
 
 public class RemoveDuplicates{
     public static void removeDuplicates(int arr[]){
@@ -14,6 +14,22 @@ public class RemoveDuplicates{
 
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,2,3,4,5};
+        int arr1[] = {1,1,2,2,3,3,4,4,5,5};
         removeDuplicates(arr);
+        System.out.println();
+        removeSortedArrayDuplicates(arr1);
+    }
+// ====================SORTED ARRAY DUPLICATES REMOVE ====================
+    public static void removeSortedArrayDuplicates(int arr1[]){
+        int i = 0;
+        for(int j =1; j<arr1.length; j++){
+            if(arr1[i]!=arr1[j]){
+                i++;
+                arr1[i] =arr1[j];
+            }
+        }
+        for(int k = 0; k<=i; k++){
+            System.out.print(arr1[k]+" ");
+        }
     }
 }
